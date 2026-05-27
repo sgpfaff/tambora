0552b94f941482d4ff72c8004c97d241de01386d
Gabriel <gabriel.pfaffman@gmail.com>
2026-05-26 23:59:30 -0400

    Update pyproject.toml: require Python 3.10 and handle ImportError for ExternalGalpyPotential

--- tambora/dynamics/forces/external_force/__init__.py
+++ tambora/dynamics/forces/external_force/__init__.py
@@ -1,1 +1,9 @@
-from .ExternalGalpyPotential import ExternalGalpyPotential
+try:
+    from .ExternalGalpyPotential import ExternalGalpyPotential
+except ImportError:
+    class ExternalGalpyPotential:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(
+                "ExternalGalpyPotential requires galpy. "
+                "Install with: pip install 'tambora[galpy]'"
+            )
